9e9e9426246079f93a709a14bd616986feb2765a
MariuszPS <k8kpol@gmail.com>
2022-04-14 08:46:19 +0200

    Queen(),Drone(),HoneyBee(),Worker()

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,12 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        output(queen.doYourJob());
      
     }
 
